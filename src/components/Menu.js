44090556f305a2cf83b5458de860cb49b5504319
eric <eric355402@yahoo.com>
2020-08-02 18:09:08 -0600

    compressed images

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,23 +1,14 @@
 import React from "react";
-
 import "../styles/menu.css";
 
 const ImageExampleFluid = () => (
   <div className="menu">
-    <img src="/images/menu1.jpg" alt="menu" />
-    <img src="/images/menu2.jpg" alt="menu" />
-    <img src="/images/menu3.jpg" alt="menu" />
-    <img src="/images/menu4.jpg" alt="menu" />
-    <img
-      src="/images/menu5.jpg"
-      style={{ transform: "rotate(-90deg)" }}
-      alt="menu"
-    />
-    <img
-      src="/images/menu6.jpg"
-      style={{ transform: "rotate(-90deg)" }}
-      alt="menu"
-    />
+    <img src="/images/menu1Comp.jpg" alt="menu" />
+    <img src="/images/menu2Comp.jpg" alt="menu" />
+    <img src="/images/menu3comp.jpg" alt="menu" />
+    <img src="/images/menu4comp.jpg" alt="menu" />
+    <img src="/images/menu5comp.jpg" alt="menu" />
+    <img src="/images/menu6comp.jpg" alt="menu" />
   </div>
 );
 

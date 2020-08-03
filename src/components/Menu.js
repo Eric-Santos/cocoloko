6e86945a3b0c8fece900949acb7c1d251d14bd6b
eric <eric355402@yahoo.com>
2020-08-03 16:41:33 -0600

    added menu box-shadow

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,13 +2,17 @@ import React from "react";
 import "../styles/menu.css";
 
 const ImageExampleFluid = () => (
-  <div className="menu">
+  <div className="menu ">
     <img src="/images/menu1Comp.jpg" alt="menu" />
     <img src="/images/menu2Comp.jpg" alt="menu" />
     <img src="/images/menu3comp.jpg" alt="menu" />
     <img src="/images/menu4comp.jpg" alt="menu" />
     <img src="/images/menu5comp.jpg" alt="menu" />
     <img src="/images/menu6comp.jpg" alt="menu" />
+    <img src="/images/menu7comp.jpg" alt="menu" />
+    <img src="/images/menu8comp.jpg" alt="menu" />
+    <img src="/images/menu9comp.jpg" alt="menu" />
+    <img src="/images/menu10comp.jpg" alt="menu" />
   </div>
 );
 

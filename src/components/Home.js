d58552a07ee97203f2fca62a12000d7865606392
eric santos <eric355402@yahoo.com>
2020-10-15 13:30:16 -0600

    fixed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,8 @@ import { Image } from "semantic-ui-react";
 
 const Home = () => {
   return (
-    <div className="main">
-      <h1>Coco Loko</h1>
+    <div className="main">  
       <Image src="/images/coverComp.jpg" size="huge" centered alt="artwork" />
-      <address>
-        <h3>9449 Montana Ave., El Paso TX Tel. 915-244-1865</h3>
-      </address>
     </div>
   );
 };

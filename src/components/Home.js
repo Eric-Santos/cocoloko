b2878e52a3d5cc97b7480edbbf2b83988a0da45d
eric santos <eric355402@yahoo.com>
2020-10-15 14:42:19 -0600

    resized home image

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="main">  
     <h3>#FugaAlCocoLoko</h3>
-      <Image src="/images/coverComp.jpg" size="huge" centered alt="artwork" />
+      <Image src="/images/coverComp.jpg" size="large" centered alt="artwork" />
     </div>
   );
 };
---
+++ src/components/notFound.js
@@ -0,0 +1,1 @@
+//add 404 page with switch and routes on app.js

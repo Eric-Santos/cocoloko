32db34f731340a8775ca0e2dfd74f45b4b4dbe0e
eric santos <eric355402@yahoo.com>
2020-10-23 14:45:56 -0600

    closing commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,13 @@ const Home = () => {
   return (
     <div className="main">
       <h3>#FugaAlCocoLoko</h3>
-      <Image src="/images/coverComp.jpg" size="large" centered alt="artwork" />
+      <Image
+        src="/images/coverComp.jpg"
+        size="large"
+        centered
+        alt="artwork"
+        classname="image"
+      />
     </div>
   );
 };
